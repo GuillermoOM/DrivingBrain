6da30b6dd0f17c23bd1776b20eaef27602d35228
GuillermoOM <guille8ameou@gmail.com>
2021-09-07 00:37:51 -0700

    finished game and integrated NN

--- main.py
+++ main.py
@@ -1,28 +1,21 @@
 # Neural Network based self driving car AI
-# Version: 0.1
+# Version: 0.5
 # Author: Guillermo Ochoa
 # TODO:
-# -Car Distance Sensors
-# -Checkpoints and fitness score
-# -NN: Finish coding
-# -NN: Interfacing car with NN
 # -NN: Generation label
 # -NN: Network weights and biases saving as json
-# -Run Multiple cars at once
 # -Able to run training mode or main mode
 
 import pygame as pg
+import neuralnet as nn
+import numpy as np
 from pygame.locals import *
 import sys
-import os
+# import os
 
 WIDTH = 1200
 HEIGHT = 900
 
-class Checkpoint(pg.sprite.Sprite):
-    def __init__(self, init_xy1, init_xy2, goal=False):
-        pg.sprite.Sprite.__init__(self)
-        
 
 class Track(pg.sprite.Sprite):
     def __init__(self):
@@ -32,53 +25,120 @@ class Track(pg.sprite.Sprite):
         self.mask = pg.mask.from_surface(self.image)
 
 
+class Checkpoints(pg.sprite.Sprite):
+    def __init__(self):
+        pg.sprite.Sprite.__init__(self)
+        self.image = pg.image.load('checkpoints.png')
+        self.rect = self.image.get_rect()
+        self.mask = pg.mask.from_surface(self.image)
+
+
 class Car(pg.sprite.Sprite):
     def __init__(self, init_x, init_y):
         pg.sprite.Sprite.__init__(self)
         self.pic = pg.image.load('car.png')
         self.image = self.pic
-        self.rect = self.image.get_rect(center = (init_x, init_y))
+        self.rect = self.image.get_rect(center=(init_x, init_y))
         self.mask = pg.mask.from_surface(self.image)
         self.x = init_x
         self.y = init_y
+        # Movement
         self.acceleration = 0.2
         self.desceleration = 0.3
         self.top_speed = 4.0
         self.rotation_speed = 2.0
         self.speed = 0.0
         self.direction = pg.math.Vector2(1.0, 0.0)
+        self.angle = 0
+        self.crashed = False
+        # Sensors
+        self.vl = pg.math.Vector2(1.0, -0.5)
+        self.vr = pg.math.Vector2(1.0, 0.5)
+        self.sfront = Sensor(0, 0)
+        self.sfront.pic = pg.transform.scale(self.sfront.pic, (200, 2))
+        self.sleft = Sensor(0, 0)
+        self.sright = Sensor(0, 0)
+        # Pointing System
+        self.score = 0
+        self.score_colliding = False
+        # Inputs
+        self.sfront.detection = False
+        self.sleft.detection = False
+        self.sright.detection = False
+        # Outputs
+        self.turn = 0
         self.accelerate = False
         self.brake = False
-        self.turn = 0
-        self.angle = 0
+        # Neural Net
+        i_weight = 0.5
+        i_bias = 0.1
+
+        self.dense1 = nn.Layer_Dense(3, 64, i_weight, i_bias)
+        self.activation1 = nn.Activation_ReLU()
+        self.dense2 = nn.Layer_Dense(64, 3, i_weight, i_bias)
+        self.activation2 = nn.Activation_Sigmoid()
 
     def update(self):
-        if (self.turn != 0):
-            self.angle -= self.turn*self.rotation_speed
-            if(self.angle > 360):self.angle=0
-            if(self.angle < 0):self.angle=360
+        if self.crashed == False:
+            # Neural Net
+            n_input = np.array([self.sfront.detection, self.sleft.detection, self.sright.detection], dtype=np.float32)
+            self.dense1.forward(n_input)
+            self.activation1.forward(self.dense1.output)
+            self.dense2.forward(self.activation1.output)
+            self.activation2.forward(self.dense2.output)
+            # print(f"forward: {self.activation2.output[0][0]}, left: {self.activation2.output[0][1]}, right: {self.activation2.output[0][2]}")
+            # print(f"forward: {self.dense2.output[0][0]}, left: {self.dense2.output[0][1]}, right: {self.dense2.output[0][2]}")
+            self.accelerate = self.activation2.output[0][0]
+            if (self.activation2.output[0][1] > self.activation2.output[0][2]):
+                self.turn = -1
+            else:
+                self.turn = 1
+            if (self.activation2.output[0][1] == self.activation2.output[0][2]):
+                self.turn = 0
 
-            self.direction.rotate_ip(self.turn*self.rotation_speed)
-            self.image, self.rect = self.rotate(self.pic, self.angle)
+            # Car Mechanics
+            if (self.turn != 0):
+                self.angle -= self.turn*self.rotation_speed
+                if(self.angle > 360):
+                    self.angle = 0
+                if(self.angle < 0):
+                    self.angle = 360
 
-        if(self.accelerate and self.speed < self.top_speed):
-            self.speed += self.acceleration
-        elif(self.brake and self.speed > 0.0):
-            self.speed -= self.desceleration
-        if(not self.brake and not self.accelerate):
-            self.speed -= 0.1
-        if(self.speed <= 0.0):
-            self.speed = 0.0
+                self.direction.rotate_ip(self.turn*self.rotation_speed)
+                self.vl.rotate_ip(self.turn*self.rotation_speed)
+                self.vr.rotate_ip(self.turn*self.rotation_speed)
+                self.image, self.rect = self.rotate(self.pic, self.angle)
+                self.mask = pg.mask.from_surface(self.image)
 
-        self.x += (self.direction * self.speed)[0]
-        self.y += (self.direction * self.speed)[1]
-        self.rect.center = int(self.x), int(self.y)
+                if self.angle == 0:
+                    self.angle_reset()
+
+            if(self.accelerate and self.speed < self.top_speed):
+                self.speed += self.acceleration
+            elif(self.brake and self.speed > 0.0):
+                self.speed -= self.desceleration
+            if(not self.brake and not self.accelerate):
+                self.speed -= 0.1
+            if(self.speed <= 0.0):
+                self.speed = 0.0
+
+            self.x += (self.direction * self.speed)[0]
+            self.y += (self.direction * self.speed)[1]
+            self.rect.center = int(self.x), int(self.y)
+            # Sensors
+            self.sfront.update(self.rect.centerx + int((self.direction*130)[0]),
+                            self.rect.centery + int((self.direction*130)[1]), self.angle)
+            self.sleft.update(self.rect.centerx + int((self.vl*70)[0]),
+                            self.rect.centery + int((self.vl*70)[1]), self.angle+20)
+            self.sright.update(self.rect.centerx + int((self.vr*70)[0]),
+                            self.rect.centery + int((self.vr*70)[1]), self.angle-20)
+            
 
     def rotate(self, image, angle):
         rot_image = pg.transform.rotate(image, angle)
         rot_rect = rot_image.get_rect(center=self.rect.center)
-        return rot_image,rot_rect
-    
+        return rot_image, rot_rect
+
     def reset(self, init_x, init_y):
         self.x = init_x
         self.y = init_y
@@ -86,6 +146,49 @@ class Car(pg.sprite.Sprite):
         self.direction = pg.math.Vector2(1.0, 0.0)
         self.angle = 0
         self.image, self.rect = self.rotate(self.pic, self.angle)
+        self.vl = pg.math.Vector2(1.0, -0.5)
+        self.vr = pg.math.Vector2(1.0, 0.5)
+
+    def angle_reset(self):
+        self.image = self.pic
+        self.rect = self.image.get_rect(center=self.rect.center)
+        self.direction = pg.math.Vector2(1.0, 0.0)
+        self.vl = pg.math.Vector2(1.0, -0.5)
+        self.vr = pg.math.Vector2(1.0, 0.5)
+        self.sfront.image = self.sfront.pic
+        self.sfront.rect = self.sfront.image.get_rect()
+        self.sleft.image = self.sleft.pic
+        self.sleft.rect = self.sleft.image.get_rect()
+        self.sright.image = self.sright.pic
+        self.sright.rect = self.sright.image.get_rect()
+
+
+class Sensor(pg.sprite.Sprite):
+    def __init__(self, init_x, init_y):
+        pg.sprite.Sprite.__init__(self)
+        self.pic = pg.image.load('sensor.png')
+        self.image = self.pic
+        self.rect = self.image.get_rect(center=(init_x, init_y))
+        self.mask = pg.mask.from_surface(self.image)
+        self.detection = False
+
+    def update(self, xpos, ypos, angle):
+        self.image, self.rect = self.rotate(self.pic, angle)
+        self.rect.center = int(xpos), int(ypos)
+        self.mask = pg.mask.from_surface(self.image)
+        if (self.detection):
+            self.pic.fill((255, 0, 0))
+        else:
+            self.pic.fill((0, 255, 0))
+
+    def rotate(self, image, angle):
+        rot_image = pg.transform.rotate(image, angle)
+        rot_rect = rot_image.get_rect(center=self.rect.center)
+        return rot_image, rot_rect
+
+def genCars(cars_group):
+    for i in range(20):
+            cars_group.add(Car(430, 150))
 
 def main():
     pg.init()
@@ -93,13 +196,19 @@ def main():
     pg.display.set_caption("Handmade Brain")
     clock = pg.time.Clock()
 
+    # Content Setup
     track = Track()
+    checkpoints = Checkpoints()
 
-    car = Car(380, 90)
+    # Score Setup
+    font = pg.font.Font(pg.font.get_default_font(), 20)
+    highest_score = 0
 
+    # Cars
+    cars = pg.sprite.Group()
+    genCars(cars)
+    
     while 1:
-        # Game Actions
-        car.mask = pg.mask.from_surface(car.image)
 
         # Input Events
         for event in pg.event.get():
@@ -107,36 +216,76 @@ def main():
                 pg.quit()
                 sys.exit()
             if event.type == KEYDOWN:
-                if event.key == K_w:
-                    car.accelerate = True
-                if event.key == K_a:
-                    car.turn = -1
-                if event.key == K_d:
-                    car.turn = 1
-                if event.key == K_s:
-                    car.brake = True
-            if event.type == KEYUP:
-                if event.key == K_w:
-                    car.accelerate = False
-                if event.key == K_a:
-                    car.turn = 0
-                if event.key == K_d:
-                    car.turn = 0
-                if event.key == K_s:
-                    car.brake = False
+                if event.key == K_r:
+                    cars.empty()
+                    genCars(cars)
+                    
+
+            # for i in cars:
+            #     if i.crashed == False:
+            #         if event.type == KEYDOWN:
+            #             if event.key == K_w:
+            #                 i.accelerate = True
+            #             if event.key == K_a:
+            #                 i.turn = -1
+            #             if event.key == K_d:
+            #                 i.turn = 1
+            #             if event.key == K_s:
+            #                 i.brake = True
+            #         if event.type == KEYUP:
+            #             if event.key == K_w:
+            #                 i.accelerate = False
+            #             if event.key == K_a:
+            #                 i.turn = 0
+            #             if event.key == K_d:
+            #                 i.turn = 0
+            #             if event.key == K_s:
+            #                 i.brake = False
 
         # Game Events
-        if pg.sprite.collide_mask(car, track):
-            print("Collision detected!")
-            car.reset(380, 90)
-
-        # Updates
-        car.update()
+        for i in cars:
+            if pg.sprite.collide_mask(i, track):
+                # i.reset(380, 90)
+                i.crashed = True
+                i.accelerate = False
+                i.turn = False
+                i.brake = False
+            if pg.sprite.collide_mask(i, checkpoints):
+                if not i.score_colliding:
+                    i.score += 10
+                    i.score_colliding = True
+            else:
+                i.score_colliding = False
+            if pg.sprite.collide_mask(i.sfront, track):
+                i.sfront.detection = True
+            else:
+                i.sfront.detection = False
+            if pg.sprite.collide_mask(i.sleft, track):
+                i.sleft.detection = True
+            else:
+                i.sleft.detection = False
+            if pg.sprite.collide_mask(i.sright, track):
+                i.sright.detection = True
+            else:
+                i.sright.detection = False
+            if i.score > highest_score:
+                highest_score = i.score
 
         # Drawing
         screen.fill((100, 100, 100))
+        screen.blit(checkpoints.image, (0, 0))
         screen.blit(track.image, (0, 0))
-        screen.blit(car.image, car.rect.topleft)
+        cars.update()
+        cars.draw(screen)
+        for i in cars:
+            if not i.crashed:
+                screen.blit(i.sfront.image, i.sfront.rect.topleft)
+                screen.blit(i.sleft.image, i.sleft.rect.topleft)
+                screen.blit(i.sright.image, i.sright.rect.topleft)
+
+        screen.blit(pg.font.Font.render(font, "Highest Score: " +
+                                        str(highest_score), True, (255, 255, 255)), (5, 10))
+
         pg.display.flip()
 
         clock.tick(60)
--- neuralnet.py
+++ neuralnet.py
@@ -3,28 +3,35 @@ import nnfs
 from nnfs.datasets import spiral_data, vertical_data
 from numpy.core.shape_base import _accumulate
 from numpy.lib.function_base import _percentile_dispatcher
+from numpy.lib.ufunclike import _deprecate_out_named_y
 
 nnfs.init()
 
+
 class Layer_Dense:
-    def __init__(self, n_inputs, n_neurons):
-        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
-        self.biases = np.zeros((1, n_neurons))
+    def __init__(self, n_inputs, n_neurons, i_weight=0.01, i_bias=0.0):
+        self.weights = i_weight * np.random.randn(n_inputs, n_neurons)
+        self.biases = i_bias * np.random.randn(1, n_neurons)
+
     def forward(self, inputs):
         self.inputs = inputs
         self.output = np.dot(inputs, self.weights) + self.biases
+
     def backward(self, dvalues):
         self.dweights = np.dot(self.inputs.T, dvalues)
         self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
         self.dinputs = np.dot(dvalues, self.weights.T)
 
+
 class Activation_ReLU:
     def forward(self, inputs):
         self.inputs = inputs
         self.output = np.maximum(0, inputs)
+
     def backward(self, dvalues):
         self.dinputs = dvalues.copy()
         self.dinputs[self.inputs <= 0] = 0
+
 
 class Activation_Softmax:
     def forward(self, inputs):
@@ -38,14 +45,31 @@ class Activation_Softmax:
 
         for index, (single_output, single_dvalues) in enumerate(zip(self.output, dvalues)):
             single_output = single_output.reshape(-1, 1)
-            jacobian_matrix = np.diagflat(single_output) - np.dot(single_output, single_output.T)
+            jacobian_matrix = np.diagflat(
+                single_output) - np.dot(single_output, single_output.T)
             self.dinputs[index] = np.dot(jacobian_matrix, single_dvalues)
+
+
+class Activation_Sigmoid:
+    # Forward pass
+    def forward(self, inputs):
+        # Save input and calculate/save output
+        # of the sigmoid function
+        self.inputs = inputs
+        self.output = 1 / (1 + np.exp(-inputs))
+    # Backward pass
+
+    def backward(self, dvalues):
+        # Derivative - calculates from output of the sigmoid function
+        self.dinputs = dvalues * (1 - self.output) * self.output
+
 
 class Loss:
     def calculate(self, output, y):
         sample_losses = self.forward(output, y)
         data_loss = np.mean(sample_losses)
         return data_loss
+
 
 class Loss_CatergoricalCrossentropy(Loss):
     def forward(self, y_pred, y_true):
@@ -64,69 +88,122 @@ class Loss_CatergoricalCrossentropy(Loss):
             )
         negative_log_likelihoods = -np.log(correct_confidences)
         return negative_log_likelihoods
-    
+
     def backward(self, dvalues, y_true):
         samples = len(dvalues)
         labels = len(dvalues[0])
 
         if len(y_true.shape) == 1:
             y_true = np.eye(labels)[y_true]
-        
+
         self.dinputs = -y_true / dvalues
         self.dinputs = self.dinputs / samples
-    
+
+
 class Activation_Softmax_Loss_CategoricalCrossentropy():
     def __init__(self):
         self.activation = Activation_Softmax()
         self.loss = Loss_CatergoricalCrossentropy()
-    
+
     def forward(self, inputs, y_true):
         self.activation.forward(inputs)
         self.output = self.activation.output
         return self.loss.calculate(self.output, y_true)
-    
+
     def backward(self, dvalues, y_true):
         samples = len(dvalues)
 
         if len(y_true.shape) == 2:
             y_true = np.argmax(y_true, axis=1)
-        
+
         self.dinputs = dvalues.copy()
         self.dinputs[range(samples), y_true] -= 1
         self.dinputs = self.dinputs / samples
 
-X, y = spiral_data(samples=100, classes=3)
 
-dense1 = Layer_Dense(2, 3)
-activation1 = Activation_ReLU()
+class Optimizer_SGD:
+    def __init__(self, Learning_rate=1., decay=0., momentum=0.):
+        self.learning_rate = Learning_rate
+        self.current_learning_rate = Learning_rate
+        self.decay = decay
+        self.iterations = 0
+        self.momentum = momentum
 
-dense2 = Layer_Dense(3, 3)
+    def pre_update_params(self):
+        if self.decay:
+            self.current_learning_rate = self.learning_rate * \
+                (1. / (1. + self.decay * self.iterations))
 
-loss_activation = Activation_Softmax_Loss_CategoricalCrossentropy()
+    def update_params(self, layer):
+        if self.momentum:
+            if not hasattr(layer, 'weight_momentums'):
+                layer.weight_momentums = np.zeros_like(layer.weights)
+                layer.bias_momentums = np.zeros_like(layer.biases)
 
-dense1.forward(X)
-activation1.forward(dense1.output)
-dense2.forward(activation1.output)
-    
-loss = loss_activation.forward(dense2.output, y)
+            weight_updates = \
+                self.momentum * layer.weight_momentums - \
+                self.current_learning_rate * layer.dweights
+            layer.weight_momentums = weight_updates
 
-print(loss_activation.output[:5])
+            bias_updates = \
+                self.momentum * layer.bias_momentums - \
+                self.current_learning_rate * layer.dbiases
+            layer.bias_momentums = bias_updates
 
-print('loss:', loss)
+        else:
+            weight_updates = -self.current_learning_rate * layer.dweights
+            bias_updates = -self.current_learning_rate * layer.dbiases
 
-predictions = np.argmax(loss_activation.output, axis=1)
-if len(y.shape) == 2:
-    y = np.argmax(y, axis=1)
-accuracy = np.mean(predictions == y)
+        layer.weights += weight_updates
+        layer.biases += bias_updates
 
-print('acc:', accuracy)
+    def post_update_params(self):
+        self.iterations += 1
 
-loss_activation.backward(loss_activation.output, y)
-dense2.backward(loss_activation.dinputs)
-activation1.backward(dense2.dinputs)
-dense1.backward(activation1.dinputs)
 
-print(dense1.dweights)
-print(dense1.dbiases)
-print(dense2.dweights)
-print(dense2.dbiases)
+if __name__ == "__main__":
+    X, y = spiral_data(samples=100, classes=3)
+
+    print(X)
+
+    dense1 = Layer_Dense(2, 64)
+
+    activation1 = Activation_ReLU()
+
+    dense2 = Layer_Dense(64, 3)
+
+    loss_activation = Activation_Softmax_Loss_CategoricalCrossentropy()
+
+    optimizer = Optimizer_SGD(Learning_rate=1, decay=1e-3, momentum=0.9)
+
+    for epoch in range(10001):
+        dense1.forward(X)
+        activation1.forward(dense1.output)
+        dense2.forward(activation1.output)
+
+        loss = loss_activation.forward(dense2.output, y)
+
+        # print('loss:', loss)
+
+        predictions = np.argmax(loss_activation.output, axis=1)
+        if len(y.shape) == 2:
+            y = np.argmax(y, axis=1)
+        accuracy = np.mean(predictions == y)
+
+        # print('acc:', accuracy)
+
+        if not epoch % 100:
+            print(f'epoch: {epoch}, ' +
+                  f'acc: {accuracy:.3f}, ' +
+                  f'loss: {loss:.3f}, ' +
+                  f'lr : {optimizer.current_learning_rate}')
+
+        loss_activation.backward(loss_activation.output, y)
+        dense2.backward(loss_activation.dinputs)
+        activation1.backward(dense2.dinputs)
+        dense1.backward(activation1.dinputs)
+
+        optimizer.pre_update_params()
+        optimizer.update_params(dense1)
+        optimizer.update_params(dense2)
+        optimizer.post_update_params()
